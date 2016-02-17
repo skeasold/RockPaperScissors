0251cef2923b91732771eb63f9cf021def2dcda2
Skylar King <sking@crazyawesome.io>
2016-02-17 00:13:19 -0600

    Added most_won counter array.

--- game.rb
+++ game.rb
@@ -1,11 +1,9 @@
 class Game
-  attr_reader :rules, :choices, :player_choice, :computer_choice
-  attr_writer :wins
-
   def play
     rules = {Rock: "Scissors", Paper: "Rock", Scissors: "Paper"}
     choices = ["Rock", "Paper", "Scissors"]
     wins = {player: 0, computer: 0, tie: 0}
+    most_won = []
 
     puts "Please type a choice."
     player_choice = gets.chomp.capitalize
@@ -16,9 +14,11 @@ class Game
       puts "PLAYER: #{player_choice} vs. COMPUTER: #{computer_choice} -- TIE"
     elsif rules.key(computer_choice).to_s == player_choice
       wins[:player] += 1
+      most_won << player_choice
       puts "PLAYER: #{player_choice} vs. COMPUTER: #{computer_choice} -- PLAYER WINS"
     else
       wins[:computer] +=1
+      most_won << computer_choice
       puts "PLAYER: #{player_choice} vs. COMPUTER: #{computer_choice} -- COMPUTER WINS"
     end
   end
