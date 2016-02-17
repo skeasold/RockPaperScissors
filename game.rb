031eb2841dbc7082552f24d2f2ccce328b5280c8
Skylar King <sking@crazyawesome.io>
2016-02-17 11:26:45 -0600

    Combined player and choice wins into one line.

--- game.rb
+++ game.rb
@@ -1,22 +1,20 @@
 class Game
   def play
-    rules = {Rock: "Scissors", Paper: "Rock", Scissors: "Paper"}
-    wins = {Player: 0, Computer: 0, Tie: 0, Rock: 0, Paper: 0, Scissors: 0}
+    rules = {rock: "scissors", paper: "rock", scissors: "paper"}
+    wins = {player: 0, computer: 0, tie: 0, rock: 0, paper: 0, scissors: 0}
     loop_count = 1000
 
     while loop_count > 0
       player_choice = rules.values.sample
       computer_choice = rules.values.sample
       if computer_choice == player_choice
-        wins[:Tie] += 1
+        wins[:tie] += 1
         puts "PLAYER: #{player_choice} vs. COMPUTER: #{computer_choice} -- TIE"
       elsif rules.key(computer_choice).to_s == player_choice
-        wins[:Player] += 1
-        wins[player_choice.to_sym] += 1
+        [:player, player_choice.to_sym].each {|sym| wins[sym] += 1}
         puts "PLAYER: #{player_choice} vs. COMPUTER: #{computer_choice} -- PLAYER WINS"
       else
-        wins[:Computer] +=1
-        wins[computer_choice.to_sym] += 1
+        [:computer, computer_choice.to_sym].each {|sym| wins[sym] += 1}
         puts "PLAYER: #{player_choice} vs. COMPUTER: #{computer_choice} -- COMPUTER WINS"
       end
       loop_count -= 1
